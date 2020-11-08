f69284d0a21db7de76b34908311874b976ea3ba5
Marek Grota <marekgro@gmail.com>
2020-11-08 15:01:36 +0100

    spring v16

--- src/main/java/pl/sda/spring_start/controller/BlogController.java
+++ src/main/java/pl/sda/spring_start/controller/BlogController.java
@@ -1,15 +1,41 @@
 package pl.sda.spring_start.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import pl.sda.spring_start.model.Post;
+import pl.sda.spring_start.service.PostService;
+import pl.sda.spring_start.service.UserService;
 
-@Controller     // klasa mapująca url na wywołanie metody i zawracająca widok html
+import java.util.Optional;
+
+@Controller     // klasa mapująca url na wywołanie metody i zwracająca widok html
 public class BlogController {
+    private UserService userService;
+    private PostService postService;
+
+    @Autowired
+    public BlogController(UserService userService, PostService postService) {
+        this.userService = userService;
+        this.postService = postService;
+    }
+
     @GetMapping("/")        // na adresie localhost:8080/
-    public String home(Model model){   // wywołaj metodę home()
-        model.addAttribute("title", "Mark's blog");
-        model.addAttribute("subtitle","The best blog in the World");
+    public String home(Model model) {   // wywołaj metodę home()
+        // dodaje atrybut do obiektu model, który może być przekazany do widoku
+        // model.addAttribute(nazwaAtrybutu, wartość);
+        model.addAttribute("posts", postService.getAllPosts());
         return "index";     // zwracającą nazwę dokumentu html który ma być wyświetlany
+    }
+
+    @GetMapping("/posts&{postId}")
+    public String getPost(@PathVariable("postId") int postId, Model model) {
+        Optional<Post> postOptional = postService.getPostById(postId);
+        postOptional.ifPresent(post -> model.addAttribute("post", post));
+        return "post";
+
+
     }
 }
