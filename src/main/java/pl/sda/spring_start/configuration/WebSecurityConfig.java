f15f5b3f4abe31dd9e24bf46e03df84f14a52635
Marek Grota <marekgro@gmail.com>
2020-11-14 15:00:02 +0100

    spring v28

--- src/main/java/pl/sda/spring_start/configuration/WebSecurityConfig.java
+++ src/main/java/pl/sda/spring_start/configuration/WebSecurityConfig.java
@@ -21,7 +21,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .usernameParameter("email")                 // nazwa pola w które wprowadzamy email (th:name)
                     .passwordParameter("password")              // nazwa pola w które wprowadzamy password (th:name)
                     .loginProcessingUrl("/login_process")       // adres na który wysyłamy dane logowania (th:action)
-                    .failureUrl("/login_error=true")                 // adres na który jesteśmy przekierowani po błędnym logowaniu
+                    .failureUrl("/login&error=true")                 // adres na który jesteśmy przekierowani po błędnym logowaniu
                     .defaultSuccessUrl("/")                     // po poprawnym logowaniu przekierowanie na adres /
                 .and()
                     .logout()                                   // wylogowanie
